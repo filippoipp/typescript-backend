8295a243804905e8cf4894dd66365e75c008118a
filippoipp <filippoipp@hotmail.com>
2020-05-28 22:18:53 -0300

    sendEmail

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
     { name: 'Filippo', email: 'filippoipp@hotmail.com'},
@@ -7,5 +8,22 @@ const users = [
 export default {
     async index(req: Request, res: Response) {
         return res.json(users);
+    },
+
+    async create(req: Request, res: Response) {
+        const emailService = new EmailService();
+
+        emailService.sendMail({
+            to: { 
+                name: 'Filippo Ippolito',
+                email: 'filippoipp@hotmail.com' 
+            },
+            message: {
+                subject: 'Bem vindo ao sistema',
+                body: 'Seja bem vindo'
+            }
+        });
+
+        return res.send();
     }
 };
